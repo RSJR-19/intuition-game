8a413f1d2d86d936f7c0f15e00cf54caefd47967
RSJR-19 <sumbadrenan619@gmail.com>
2025-07-23 15:47:26 +0800

    flipping cards inside cards??

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const theCard = document.getElementById("theCard");
 const frontFaceScreen = document.getElementById("frontFaceScreen");
 const easyLevel = document.getElementById("easyLevel");
+const leftCardEasy = document.getElementById("leftCardEasy");
+const rightCardEasy = document.getElementById("rightCardEasy");
+const lowerCardEasy = document.getElementById("lowerCardEasy");
 
 
 frontFaceScreen.style.display = "flex";
 easyLevel.style.display = "none";
+
+function normalFlip(idTarget) {
+    idTarget.style.transform = "rotateY(-180deg)";
+    setTimeout(() => {
+    if (idTarget === leftCardEasy) {
+        rightCardEasy.style.transform = "rotateY(-180deg)";
+        lowerCardEasy.style.transform = "rotateY(-180deg)";
+    }
+    else if (idTarget === rightCardEasy) {
+        leftCardEasy.style.transform = "rotateY(-180deg)";
+        lowerCardEasy.style.transform = "rotateY(-180deg)";
+    }
+    else if (idTarget === lowerCardEasy) {
+        leftCardEasy.style.transform = "rotateY(-180deg)";
+        rightCardEasy.style.transform = "rotateY(-180deg)";
+    }
+},1000)
+        
+
+
+
+
+}
+
+function flipAll() {
+
+
+}
 
 function flip() {
     
